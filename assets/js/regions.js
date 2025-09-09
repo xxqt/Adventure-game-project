fa1210df7fcb8ce9424f49f2e79e54175a4932e9
Hien Ho <bvgkw777@gmail.com>
2025-09-09 17:08:09 -0500

    updated battle.html

---
+++ assets/js/regions.js
@@ -0,0 +1,20 @@
+const regions = {
+  forest: {
+    label: "Forest",
+    overworldBg: "../assets/overworld.png",
+    battleBg:    "../assets/forest_bg.jpg",
+    monstersByName: ["Goblin", "Flying Eye"]  // names from enemies.js
+  },
+  temple: {
+    label: "Temple",
+    overworldBg: "../assets/overworld.png",
+    battleBg:    "../assets/temple_bg.png",
+    monstersByName: ["Skeleton", "Flying Eye"]
+  },
+  shop: {
+    label: "Shop",
+    overworldBg: "../assets/overworld.png",
+    battleBg:    "../assets/shop_bg.png",
+    monstersByName: [] // safe zone: no battles
+  }
+};
